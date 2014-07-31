27a84ddbfc496bff30f666810eb41cd9bb1f07ab
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-07-31 15:45:13 +0400

    fixes for main page

--- bitrix/templates/main/header.php
+++ bitrix/templates/main/header.php
@@ -48,7 +48,7 @@
 			require(['basics/get_val'], function (getVal) {
 				getVal.set('clientSide', true);
 				getVal.set('lang', '<?=LANGUAGE_ID?>');
-				getVal.set('revision', '<?=$revision?>'); // temporary development mode
+				getVal.set('revision', '<?=$revision?>');
 				getVal.set('tplPath', '<?=$tplPath?>');
 				require(['main']);
 			});
@@ -58,11 +58,11 @@
 
 <body><?$APPLICATION->ShowPanel()?>
 	<header><?
-        if($APPLICATION->GetCurPage != SITE_DIR){?>
+        if($APPLICATION->GetCurPage(0) != SITE_DIR){?>
             <a class="logo" href="<?=SITE_DIR?>" title="<?=GetMessage("GOTO_MAINPAGE")?>"><?
         }?>
 			<img class="logo" alt="<?=GetMessage("MAIN_SLOGAN")?>" src="<?=$tplPath?>/images/header_logo.png" /><?
-        if($APPLICATION->GetCurPage != SITE_DIR){?>
+        if($APPLICATION->GetCurPage(0) != SITE_DIR){?>
             </a><?
         }?>
 		<nav class="menu"><?$APPLICATION->IncludeComponent("bitrix:menu", "menu.main", Array(
@@ -110,54 +110,81 @@
                     <img class="logo" alt="<?=GetMessage("MAIN_SLOGAN")?>" src="<?=$tplPath?>/images/main_page/logo.png" />
                     <div class="istablish"><?=GetMessage("BASED_AT")?></div>
                 </div>
-                <div class="slider"><?$APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"news.list.slider",
-	Array(
-		"IBLOCK_TYPE" => "service",
-		"IBLOCK_ID" => "2",
-		"NEWS_COUNT" => "1000",
-		"SORT_BY1" => "SORT",
-		"SORT_ORDER1" => "ASC",
-		"SORT_BY2" => "",
-		"SORT_ORDER2" => "",
-		"FILTER_NAME" => "",
-		"FIELD_CODE" => array("ID", "CODE", "XML_ID", "NAME", "TAGS", "SORT", "PREVIEW_TEXT", "PREVIEW_PICTURE", "DETAIL_TEXT", "DETAIL_PICTURE", "DATE_ACTIVE_FROM", "ACTIVE_FROM", "DATE_ACTIVE_TO", "ACTIVE_TO", "SHOW_COUNTER", "SHOW_COUNTER_START", "IBLOCK_TYPE_ID", "IBLOCK_ID", "IBLOCK_CODE", "IBLOCK_NAME", "IBLOCK_EXTERNAL_ID", "DATE_CREATE", "CREATED_BY", "CREATED_USER_NAME", "TIMESTAMP_X", "MODIFIED_BY", "USER_NAME", ""),
-		"PROPERTY_CODE" => array("", ""),
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"AJAX_OPTION_HISTORY" => "N",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "7200",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"ACTIVE_DATE_FORMAT" => "",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-		"ADD_SECTIONS_CHAIN" => "N",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"DISPLAY_DATE" => "N",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "N",
-		"DISPLAY_PREVIEW_TEXT" => "N",
-		"PAGER_TEMPLATE" => "",
-		"DISPLAY_TOP_PAGER" => "N",
-		"DISPLAY_BOTTOM_PAGER" => "N",
-		"PAGER_TITLE" => "Новости",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N"
-	)
-);?><br>
+                <div class="slider"><?$APPLICATION->IncludeComponent("bitrix:news.list", "news.list.slider", array(
+	"IBLOCK_TYPE" => "service",
+	"IBLOCK_ID" => "2",
+	"NEWS_COUNT" => "1000",
+	"SORT_BY1" => "SORT",
+	"SORT_ORDER1" => "ASC",
+	"SORT_BY2" => "",
+	"SORT_ORDER2" => "",
+	"FILTER_NAME" => "",
+	"FIELD_CODE" => array(
+		0 => "ID",
+		1 => "CODE",
+		2 => "XML_ID",
+		3 => "NAME",
+		4 => "TAGS",
+		5 => "SORT",
+		6 => "PREVIEW_TEXT",
+		7 => "PREVIEW_PICTURE",
+		8 => "DETAIL_TEXT",
+		9 => "DETAIL_PICTURE",
+		10 => "DATE_ACTIVE_FROM",
+		11 => "ACTIVE_FROM",
+		12 => "DATE_ACTIVE_TO",
+		13 => "ACTIVE_TO",
+		14 => "SHOW_COUNTER",
+		15 => "SHOW_COUNTER_START",
+		16 => "IBLOCK_TYPE_ID",
+		17 => "IBLOCK_ID",
+		18 => "IBLOCK_CODE",
+		19 => "IBLOCK_NAME",
+		20 => "IBLOCK_EXTERNAL_ID",
+		21 => "DATE_CREATE",
+		22 => "CREATED_BY",
+		23 => "CREATED_USER_NAME",
+		24 => "TIMESTAMP_X",
+		25 => "MODIFIED_BY",
+		26 => "USER_NAME",
+		27 => "",
+	),
+	"PROPERTY_CODE" => array(
+		0 => "",
+		1 => "",
+	),
+	"CHECK_DATES" => "Y",
+	"DETAIL_URL" => "",
+	"AJAX_MODE" => "N",
+	"AJAX_OPTION_JUMP" => "N",
+	"AJAX_OPTION_STYLE" => "N",
+	"AJAX_OPTION_HISTORY" => "N",
+	"CACHE_TYPE" => "A",
+	"CACHE_TIME" => "7200",
+	"CACHE_FILTER" => "N",
+	"CACHE_GROUPS" => "Y",
+	"PREVIEW_TRUNCATE_LEN" => "",
+	"ACTIVE_DATE_FORMAT" => "",
+	"SET_STATUS_404" => "N",
+	"SET_TITLE" => "N",
+	"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+	"ADD_SECTIONS_CHAIN" => "N",
+	"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+	"PARENT_SECTION" => "",
+	"PARENT_SECTION_CODE" => "",
+	"INCLUDE_SUBSECTIONS" => "Y",
+	"PAGER_TEMPLATE" => "",
+	"DISPLAY_TOP_PAGER" => "N",
+	"DISPLAY_BOTTOM_PAGER" => "N",
+	"PAGER_TITLE" => "",
+	"PAGER_SHOW_ALWAYS" => "N",
+	"PAGER_DESC_NUMBERING" => "N",
+	"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+	"PAGER_SHOW_ALL" => "N",
+	"AJAX_OPTION_ADDITIONAL" => ""
+	),
+	false
+);?>
                 </div>
                 <a class="next_card"><span></span></a>
             </section>

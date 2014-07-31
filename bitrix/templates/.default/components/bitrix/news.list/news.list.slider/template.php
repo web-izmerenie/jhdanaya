c069e5486b59882f793cfc12d646988a9968835a
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-07-31 16:03:05 +0400

    front-end edit for slider on main page

--- bitrix/templates/.default/components/bitrix/news.list/news.list.slider/template.php
+++ bitrix/templates/.default/components/bitrix/news.list/news.list.slider/template.php
@@ -1,5 +1,9 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 <?foreach($arResult["ITEMS"] as $arItem):?>
-    <img src="<?=$arItem["DETAIL_PICTURE"]["SRC"]?>" alt="<?=$arItem["NAME"]?>" />
+	<?
+	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+    <img id="<?=$this->GetEditAreaId($arItem['ID']);?>" src="<?=$arItem["DETAIL_PICTURE"]["SRC"]?>" alt="<?=$arItem["NAME"]?>" />
 <?endforeach;?>

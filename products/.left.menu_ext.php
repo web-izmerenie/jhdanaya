447e227cf2322f6242283ec5b3142814e2b79877
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-18 00:47:03 +0400

    refactoring of products/.left.menu_ext.php

--- products/.left.menu_ext.php
+++ products/.left.menu_ext.php
@@ -5,52 +5,58 @@ $page = $APPLICATION->GetCurPage();
 
 $arDir = explode("/", $dir);
 foreach($arDir as $k => $v){
-    if($v === "") unset($arDir[$k]);
+	if($v === "") unset($arDir[$k]);
 }
 
-if((count($arDir) > 1 || $_GET["show"] === "all") && ($page != "/products/women/" && $page != "/products/men/" && $page != "/products/cildren/")){
-    CModule::IncludeModule("iblock");
-    $aMenuLinks = array();
-    $section = CIBlockSection::GetList(
-        array(
-            "SORT" => "asc"
-        ),
-        array(
-            "IBLOCK_CODE" => "catalog",
-            "PROPERTY" => array(
-                "BRAND" => false
-            )
-        ),
-        true
-        
-    );
-    if($section->SelectedRowsCount()){
-        while($arSection = $section->GetNExt()){
-            if($arSection["ELEMENT_CNT"]){
-                if(stripos($APPLICATION->GetCurDir(), "/women/"))
-                    $arSection["SECTION_PAGE_URL"] = str_replace("products", "products/women", $arSection["SECTION_PAGE_URL"]);
-                if(stripos("/men/", $APPLICATION->GetCurDir()))
-                    $arSection["SECTION_PAGE_URL"] = str_replace("products", "products/men", $arSection["SECTION_PAGE_URL"]);
-                if(stripos("/children/", $APPLICATION->GetCurDir()))
-                    $arSection["SECTION_PAGE_URL"] = str_replace("products", "products/children", $arSection["SECTION_PAGE_URL"]);
-                    
-                $aMenuLinks[] = Array(
-                    $arSection["NAME"], 
-                    $arSection["SECTION_PAGE_URL"], 
-                    array(), 
-                    array(), 
-                    "" 
-                );
-            }
-        }
-        $aMenuLinks[] = Array(
-            GetMessage("ALL"), 
-            $arDir[0] . "?show=all", 
-            array(), 
-            array(), 
-            "" 
-        );
-    }
+if(
+	(count($arDir) > 1 || $_GET["show"] === "all") &&
+	(
+		$page != "/products/women/" &&
+		$page != "/products/men/" &&
+		$page != "/products/cildren/"
+	)
+){
+	CModule::IncludeModule("iblock");
+	$aMenuLinks = array();
+	$section = CIBlockSection::GetList(
+		array(
+			"SORT" => "asc"
+		),
+		array(
+			"IBLOCK_CODE" => "catalog",
+			"PROPERTY" => array(
+				"BRAND" => false
+			)
+		),
+		true
+	);
+	if($section->SelectedRowsCount()){
+		while($arSection = $section->GetNExt()){
+			if($arSection["ELEMENT_CNT"]){
+				if(stripos($APPLICATION->GetCurDir(), "/women/"))
+					$arSection["SECTION_PAGE_URL"] = str_replace("products", "products/women", $arSection["SECTION_PAGE_URL"]);
+				if(stripos("/men/", $APPLICATION->GetCurDir()))
+					$arSection["SECTION_PAGE_URL"] = str_replace("products", "products/men", $arSection["SECTION_PAGE_URL"]);
+				if(stripos("/children/", $APPLICATION->GetCurDir()))
+					$arSection["SECTION_PAGE_URL"] = str_replace("products", "products/children", $arSection["SECTION_PAGE_URL"]);
+
+				$aMenuLinks[] = Array(
+					$arSection["NAME"],
+					$arSection["SECTION_PAGE_URL"],
+					array(),
+					array(),
+					""
+				);
+			}
+		}
+		$aMenuLinks[] = Array(
+			GetMessage("ALL"),
+			$arDir[0] . "?show=all",
+			array(),
+			array(),
+			""
+		);
+	}
 }
 
 ?>

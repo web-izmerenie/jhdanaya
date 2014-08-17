cd687d2c9418d3062dc15c0105b70bdc4eab396f
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-17 21:00:57 +0400

    updated ajax-handler

--- ajax/handler.php
+++ ajax/handler.php
@@ -1,6 +1,8 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
+$json["dedug"] = $_GET;
+
 CModule::IncludeModule("iblock");
 $iblock = CIBlock::GetList(
     array(),
@@ -54,24 +56,35 @@ $currentcount = ($_GET["page"] - 1) * $_GET["count"];
 if($pagecount){
     $json["status"] = "success";    
     $items = array();
-    while($arRes = $res->GetNext()){
+    while($arRes = $res->GetNextElement()){
+        $arResF = $arRes->GetFields();
+        $arResP = $arRes->GetProperties();
         if($currentcount >= $totalcount){
             $json["status"] = "end_of_list";
             break;
         }
         $sizeParams = $_GET["brand"] ? array("width" => "288", "height" => "288") : array("width" => "231", "height" => "126");
-        $thumb = CFile::ResizeImageGet($arRes["PREVIEW_PICTURE"], $sizeParams, BX_RESIZE_IMAGE_PROPORTIONAL);
+        $thumb = CFile::ResizeImageGet($arResF["PREVIEW_PICTURE"], $sizeParams, BX_RESIZE_IMAGE_PROPORTIONAL);
         
+        $shop = CIBlockElement::GetById($arResP["SHOP"]["VALUE"]);
+        if($shop->SelectedRowsCount()){
+            $arShop = $shop->GetNextElement();
+            $shopFields = $arShop->GetFields();
+            $shopProps = $arShop->GetProperties();
+        }
         $items[$_GET["iblock"]][] = array(
-            "id" => "bx_id_".$arRes["ID"],
+            "id" => "bx_id_".$arResF["ID"],
             "preview" => array(
-                "description" => $arRes["NAME"],
+                "description" => $arResF["NAME"],
                 "src" => $thumb["src"]
             ),
             "info" => array(
-                "text" => $arRes["PREVIEW_TEXT"],
+                "text" => "
+                <p>Арт. ".$arResP["ARTICLE"]["VALUE"]."</p>
+                <p>".$arResF["PREVIEW_TEXT"]."</p>
+                <p>".$shopFields["NAME"]."<br />".$shopProps["PHONE"]["VALUE"]."</p>",
                 "picture" => array(
-                    "description" => $arRes["NAME"],
+                    "description" => $arResF["NAME"],
                     "src" => $thumb["src"]
                 )
             )

b98c9594ca51040aacc7be86ef63ee1d911b786d
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-18 01:17:22 +0400

    refactoring of detail events page template

--- bitrix/templates/main/components/bitrix/news.detail/news.detail.events/template.php
+++ bitrix/templates/main/components/bitrix/news.detail/news.detail.events/template.php
@@ -1,34 +1,25 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<section class="event_detail" itemscope itemtype="http://schema.org/NewsArticle"><?
-    $arResult["DISPLAY_ACTIVE_FROM"] = preg_split("/\s/", $arResult["DISPLAY_ACTIVE_FROM"]);?>
+<section class="event_detail" itemscope itemtype="http://schema.org/NewsArticle">
+	<?$arResult["DISPLAY_ACTIVE_FROM"] = preg_split("/\s/", $arResult["DISPLAY_ACTIVE_FROM"]);?>
 	<div class="date" itemprop="dateline"><?=$arResult["DISPLAY_ACTIVE_FROM"][0]?> <?=$arResult["DISPLAY_ACTIVE_FROM"][1]?></div>
+
 	<div class="preview_text" itemprop="description">
-        <?if($arResult["DETAIL_TEXT"]):?>
-            <?=$arResult["DETAIL_TEXT"]?>
-        <?else:?>
-            <?=$arResult["PREVIEW_TEXT"]?>
-        <?endif;?>
-	</div><?
-    if($arResult["DISPLAY_PROPERTIES"]["GALLERY"]["VALUE"]){?>
-    <ul class="preview_photos"><?
-        $ar_ids = $arResult["DISPLAY_PROPERTIES"]["GALLERY"]["VALUE"];
-        foreach($ar_ids as $id){
-            $thumb = CFile::ResizeImageGet($id, array("width" => "233", "height" => "233"), BX_RESIZE_IMAGE_PROPORTIONAL);?>
-            <li><img alt="" src="<?=$thumb["src"]?>" itemprop="image" /></li><?
-        }?>
-    </ul><?
-    }?>
+		<?if($arResult["DETAIL_TEXT"]):?>
+			<?=$arResult["DETAIL_TEXT"]?>
+		<?else:?>
+			<?=$arResult["PREVIEW_TEXT"]?>
+		<?endif;?>
+	</div>
+
+	<?if($arResult["DISPLAY_PROPERTIES"]["GALLERY"]["VALUE"]){?>
+		<ul class="preview_photos">
+			<?$ar_ids = $arResult["DISPLAY_PROPERTIES"]["GALLERY"]["VALUE"];?>
+			<?foreach($ar_ids as $id){?>
+				<?$thumb = CFile::ResizeImageGet($id, array("width" => "233", "height" => "233"), BX_RESIZE_IMAGE_PROPORTIONAL);?>
+				<li><img alt="" src="<?=$thumb["src"]?>" itemprop="image" /></li>
+			<?}?>
+		</ul>
+	<?}?>
 </section>

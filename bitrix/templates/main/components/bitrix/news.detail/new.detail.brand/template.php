b523397359860ac9a0146bc1e10206f8580dec85
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-17 23:35:11 +0400

    fixed unvalid shit of brands and some refactoring

--- bitrix/templates/main/components/bitrix/news.detail/new.detail.brand/template.php
+++ bitrix/templates/main/components/bitrix/news.detail/new.detail.brand/template.php
@@ -20,83 +20,83 @@ $this->setFrameMode(true);
 			<?=GetMessage("FOUNDATION_PLACE")?> — <?=$arResult["DISPLAY_PROPERTIES"]["PLACE"]["VALUE"]?>
 		</p>
 	</section><?
-    if($arResult["DISPLAY_PROPERTIES"]["Q_PICTURE"]["VALUE"] || $arResult["DISPLAY_PROPERTIES"]["Q_TEXT"]["VALUE"]){?>
+	if($arResult["DISPLAY_PROPERTIES"]["Q_PICTURE"]["VALUE"] || $arResult["DISPLAY_PROPERTIES"]["Q_TEXT"]["VALUE"]){?>
 	<blockquote><?
-        if($arResult["DISPLAY_PROPERTIES"]["Q_PICTURE"]["VALUE"]){
-            $ar_q_picture = CFile::GetFileArray($arResult["DISPLAY_PROPERTIES"]["Q_PICTURE"]["VALUE"]);
-            if($ar_q_picture["WIDTH"] > 118){
-                $q_picture = CFile::ResizeImageGet($arResult["DISPLAY_PROPERTIES"]["Q_PICTURE"]["VALUE"], array("width" => "118", "height" => "118"), BX_RESIZE_IMAGE_PROPORTIONAL);
-            }else{
-                $q_picture["src"] = CFile::GetPath($arResult["DISPLAY_PROPERTIES"]["Q_PICTURE"]["VALUE"]);
-            }?>
-            <img alt="<?=$arResult["DISPLAY_PROPERTIES"]["Q_AUTHOR"]["VALUE"]?>" src="<?=$q_picture["src"]?>" /><?
-        }?>
+		if($arResult["DISPLAY_PROPERTIES"]["Q_PICTURE"]["VALUE"]){
+			$ar_q_picture = CFile::GetFileArray($arResult["DISPLAY_PROPERTIES"]["Q_PICTURE"]["VALUE"]);
+			if($ar_q_picture["WIDTH"] > 118){
+				$q_picture = CFile::ResizeImageGet($arResult["DISPLAY_PROPERTIES"]["Q_PICTURE"]["VALUE"], array("width" => "118", "height" => "118"), BX_RESIZE_IMAGE_PROPORTIONAL);
+			}else{
+				$q_picture["src"] = CFile::GetPath($arResult["DISPLAY_PROPERTIES"]["Q_PICTURE"]["VALUE"]);
+			}?>
+			<img alt="<?=$arResult["DISPLAY_PROPERTIES"]["Q_AUTHOR"]["VALUE"]?>" src="<?=$q_picture["src"]?>" /><?
+		}?>
 		<?=$arResult["DISPLAY_PROPERTIES"]["Q_TEXT"]["~VALUE"]["TEXT"]?>
 		<p class="signature"><?=$arResult["DISPLAY_PROPERTIES"]["Q_AUTHOR"]["VALUE"]?></p>
 	</blockquote><?
-    }?>
+	}?>
 
 	<section class="accent_info_block"><?
-        if($arResult["DETAIL_PICTURE"]){?>
-            <div class="col_l picture" style="background-image:url('<?=$arResult["DETAIL_PICTURE"]["SRC"]?>');"></div><?
-        }?><?
-        if($arResult["DETAIL_TEXT"]){?>
+		if($arResult["DETAIL_PICTURE"]){?>
+			<div class="col_l picture" style="background-image:url('<?=$arResult["DETAIL_PICTURE"]["SRC"]?>');"></div><?
+		}?><?
+		if($arResult["DETAIL_TEXT"]){?>
 		<div class="col_r info">
 			<?=$arResult["DETAIL_TEXT"]?>
 		</div><?
-        }?>
+		}?>
 	</section>
 	<section class="info_right_pic"><?
-        if($arResult["DISPLAY_PROPERTIES"]["BL1_PICTURE"]["VALUE"]){
-            $ar_bl1_picture = CFile::GetFileArray($arResult["DISPLAY_PROPERTIES"]["BL1_PICTURE"]["VALUE"]);
-            if($ar_bl1_picture["WIDTH"] > 304){
-                $bl1_picture = CFile::ResizeImageGet($arResult["DISPLAY_PROPERTIES"]["BL1_PICTURE"]["VALUE"], array("width" => "304", "height" => "304"), BX_RESIZE_IMAGE_PROPORTIONAL);
-            }else{
-                $bl1_picture["src"] = CFile::GetPath($arResult["DISPLAY_PROPERTIES"]["BL1_PICTURE"]["VALUE"]);
-            }?>
-            <img src="<?=$bl1_picture["src"]?>" /><?
-        }?><?
-        if($arResult["DISPLAY_PROPERTIES"]["BL1_TEXT"]["~VALUE"]["TEXT"]){?>
+		if($arResult["DISPLAY_PROPERTIES"]["BL1_PICTURE"]["VALUE"]){
+			$ar_bl1_picture = CFile::GetFileArray($arResult["DISPLAY_PROPERTIES"]["BL1_PICTURE"]["VALUE"]);
+			if($ar_bl1_picture["WIDTH"] > 304){
+				$bl1_picture = CFile::ResizeImageGet($arResult["DISPLAY_PROPERTIES"]["BL1_PICTURE"]["VALUE"], array("width" => "304", "height" => "304"), BX_RESIZE_IMAGE_PROPORTIONAL);
+			}else{
+				$bl1_picture["src"] = CFile::GetPath($arResult["DISPLAY_PROPERTIES"]["BL1_PICTURE"]["VALUE"]);
+			}?>
+			<img alt="<?=$arResult["DISPLAY_PROPERTIES"]["BL1_PICTURE"]["DESCRIPTION"]?>" src="<?=$bl1_picture["src"]?>" /><?
+		}?><?
+		if($arResult["DISPLAY_PROPERTIES"]["BL1_TEXT"]["~VALUE"]["TEXT"]){?>
 		<?=$arResult["DISPLAY_PROPERTIES"]["BL1_TEXT"]["~VALUE"]["TEXT"]?><?
-        }?>
+		}?>
 	</section>
 	<div class="figure_split"></div>
 
 	<section class="two_column_info"><?
-        if($arResult["DISPLAY_PROPERTIES"]["BL2_CODE"]["VALUE"]){?>
-            <div class="col_l">
-                <div class="youtube_video" data-youtube-id="<?=$arResult["DISPLAY_PROPERTIES"]["BL2_CODE"]["VALUE"]?>"></div>
-            </div><?
-        }?><?
-        if($arResult["DISPLAY_PROPERTIES"]["BL2_TEXT"]["~VALUE"]["TEXT"]){?>
-            <div class="col_r">
-                <?=$arResult["DISPLAY_PROPERTIES"]["BL2_TEXT"]["~VALUE"]["TEXT"]?>
-            </div><?
-        }?>
+		if($arResult["DISPLAY_PROPERTIES"]["BL2_CODE"]["VALUE"]){?>
+			<div class="col_l">
+				<div class="youtube_video" data-youtube-id="<?=$arResult["DISPLAY_PROPERTIES"]["BL2_CODE"]["VALUE"]?>"></div>
+			</div><?
+		}?><?
+		if($arResult["DISPLAY_PROPERTIES"]["BL2_TEXT"]["~VALUE"]["TEXT"]){?>
+			<div class="col_r">
+				<?=$arResult["DISPLAY_PROPERTIES"]["BL2_TEXT"]["~VALUE"]["TEXT"]?>
+			</div><?
+		}?>
 	</section><?
-    if($arResult["DISPLAY_PROPERTIES"]["PROMO_SLOGAN"]["~VALUE"]["TEXT"]){?>
-        <section class="notation">
-            <?=$arResult["DISPLAY_PROPERTIES"]["PROMO_SLOGAN"]["~VALUE"]["TEXT"]?>
-        </section><?
-    }?><?
-    if(count($arResult["DISPLAY_PROPERTIES"]["PROMO_GALLERY"]["VALUE"])){?>
-        <!-- превьюшки должны ограничиваться по ширине в 140, по высоте в 200 -->
-        <ul class="production_list"><?
-            foreach($arResult["DISPLAY_PROPERTIES"]["PROMO_GALLERY"]["VALUE"] as $id){
-                $ar_id = CFile::GetFileArray($id);
-                $bx_resize_image = $ar_id["WIDTH"] > $ar_id["HEIGHT"] ? "BX_RESIZE_IMAGE_PROPORTIONAL" : "BX_RESIZE_IMAGE_PROPORTIONAL_ALT";
-                $thumd = CFile::ResizeImageGet($id, array("width" => "140", "height" => "200"), $bx_resize_image);?>
-                <li><img src="<?=$thumd["src"]?>" /></li><?
-            }?>
-        </ul><?
-    }?>
+	if($arResult["DISPLAY_PROPERTIES"]["PROMO_SLOGAN"]["~VALUE"]["TEXT"]){?>
+		<section class="notation">
+			<?=$arResult["DISPLAY_PROPERTIES"]["PROMO_SLOGAN"]["~VALUE"]["TEXT"]?>
+		</section><?
+	}?><?
+	if(count($arResult["DISPLAY_PROPERTIES"]["PROMO_GALLERY"]["VALUE"])){?>
+		<!-- превьюшки должны ограничиваться по ширине в 140, по высоте в 200 -->
+		<ul class="production_list"><?
+			foreach($arResult["DISPLAY_PROPERTIES"]["PROMO_GALLERY"]["VALUE"] as $id){
+				$ar_id = CFile::GetFileArray($id);
+				$bx_resize_image = $ar_id["WIDTH"] > $ar_id["HEIGHT"] ? "BX_RESIZE_IMAGE_PROPORTIONAL" : "BX_RESIZE_IMAGE_PROPORTIONAL_ALT";
+				$thumd = CFile::ResizeImageGet($id, array("width" => "140", "height" => "200"), $bx_resize_image);?>
+				<li><img alt="" src="<?=$thumd["src"]?>" /></li><?
+			}?>
+		</ul><?
+	}?>
 	<section class="back_link"><?
-        $ar_small_picture = CFile::GetFileArray($arResult["DISPLAY_PROPERTIES"]["SMALL_PICTURE"]["VALUE"]);
-        if($ar_small_picture["WIDTH"] > 57){
-            $small_picture = CFile::ResizeImageGet($arResult["DISPLAY_PROPERTIES"]["SMALL_PICTURE"]["VALUE"], array("width" => "57", "height" => "57"), BX_RESIZE_IMAGE_PROPORTIONAL);
-        }else{
-            $small_picture["src"] = CFile::GetPath($arResult["DISPLAY_PROPERTIES"]["SMALL_PICTURE"]["VALUE"]);
-        }?>        
+		$ar_small_picture = CFile::GetFileArray($arResult["DISPLAY_PROPERTIES"]["SMALL_PICTURE"]["VALUE"]);
+		if($ar_small_picture["WIDTH"] > 57){
+			$small_picture = CFile::ResizeImageGet($arResult["DISPLAY_PROPERTIES"]["SMALL_PICTURE"]["VALUE"], array("width" => "57", "height" => "57"), BX_RESIZE_IMAGE_PROPORTIONAL);
+		}else{
+			$small_picture["src"] = CFile::GetPath($arResult["DISPLAY_PROPERTIES"]["SMALL_PICTURE"]["VALUE"]);
+		}?>
 		<a href="/brand/<?=$arResult["CODE"]?>/?show=all">
 			<img alt="" src="<?=$small_picture["src"]?>" />
 			<span><?=GetMessage("COLLECTION_BRAND")?></span>
--- bitrix/templates/main/components/bitrix/news/news.brandproducts/bitrix/news.list/.default/template.php
+++ bitrix/templates/main/components/bitrix/news/news.brandproducts/bitrix/news.list/.default/template.php
@@ -5,79 +5,83 @@ $itemCount = 6;
 $onPage = count($arResult["ITEMS"]);
 global $arrFilter, $currentBrendID;
 ?>
-<ul class="collection_list brand"><?
-foreach($arResult["ITEMS"] as $Item){?>
-    <li id="bx_id_<?=$Item["ID"]?>"><?
-        if($Item["PREVIEW_PICTURE"]){?>
-            <a href="<?=$Item["DETAIL_PICTURE"]["SRC"]?>" class="preview">
-                <img src="<?=$Item["PREVIEW_PICTURE"]["SRC"]?>" />
-            </a><?
-        }?><?
-        if($Item["PREVIEW_TEXT"]){?>
-            <div class="info">
-                <div class="text">
-                    <p><?=GetMessage("ART.")?>&nbsp;<?=$Item["DISPLAY_PROPERTIES"]["ARTICLE"]["VALUE"]?></p>
-                    <p><?=$Item["PREVIEW_TEXT"]?></p><?
-                    if($Item["DISPLAY_PROPERTIES"]["SHOP"]["VALUE"]){
-                        $shop = CIBlockElement::GetById($Item["DISPLAY_PROPERTIES"]["SHOP"]["VALUE"]);
-                        $arShop = $shop->GetNextElement();
-                        $shopFields = $arShop->GetFields();
-                        $shopProps = $arShop->GetProperties();?>
-                        <p><?=$shopFields["NAME"]?><br /><?=$shopProps["PHONE"]["VALUE"]?></p><?
-                    }?>
-                </div><?
-                if($Item["DETAIL_PICTURE"]){?>  
-                    <img class="picture" alt="" src="<?=$Item["DETAIL_PICTURE"]["SRC"]?>" /><?
-                }?> 
-            </div><?
-        }?>    
-    </li><?
-}?>
-</ul><?
+<ul class="collection_list brand">
+<?foreach($arResult["ITEMS"] as $Item){?>
+	<li id="bx_id_<?=$Item["ID"]?>">
+		<?if($Item["PREVIEW_PICTURE"]){?>
+			<a href="<?=$Item["DETAIL_PICTURE"]["SRC"]?>" class="preview">
+				<img alt="<?=$Item["PREVIEW_PICTURE"]["DESCRIPTION"]?>" src="<?=$Item["PREVIEW_PICTURE"]["SRC"]?>" />
+			</a>
+		<?}?>
+		<?if($Item["PREVIEW_TEXT"]){?>
+			<div class="info">
+				<div class="text">
+					<p><?=GetMessage("ART.")?>&nbsp;<?=$Item["DISPLAY_PROPERTIES"]["ARTICLE"]["VALUE"]?></p>
+					<?=$Item["PREVIEW_TEXT"]?>
+					<?if($Item["DISPLAY_PROPERTIES"]["SHOP"]["VALUE"]){?>
+						<?$shop = CIBlockElement::GetById($Item["DISPLAY_PROPERTIES"]["SHOP"]["VALUE"]);
+						$arShop = $shop->GetNextElement();
+						$shopFields = $arShop->GetFields();
+						$shopProps = $arShop->GetProperties();?>
+						<p><?=$shopFields["NAME"]?><br /><?=$shopProps["PHONE"]["VALUE"]?></p>
+					<?}?>
+				</div>
+				<?if($Item["DETAIL_PICTURE"]){?>
+					<img class="picture" alt="<?=$Item["DETAIL_PICTURE"]["DESCRIPTION"]
+						?>" src="<?=$Item["DETAIL_PICTURE"]["SRC"]?>" />
+				<?}?>
+			</div>
+		<?}?>
+	</li>
+<?}?>
+</ul>
 
-$arFilter = array(
-    "ACTIVE" => "Y",
-    "IBLOCK_ID" => $arResult["ID"],
-    
+<?$arFilter = array(
+	"ACTIVE" => "Y",
+	"IBLOCK_ID" => $arResult["ID"],
 );
 if($arResult["SECTION"]["PATH"][0]["ID"]){
-    $arFilter["SECTION_ID"] = $arResult["SECTION"]["PATH"][0]["ID"];
+	$arFilter["SECTION_ID"] = $arResult["SECTION"]["PATH"][0]["ID"];
 }else{
-    $path = $APPLICATION->GetCurPage();
-    $path = explode("/", $path);
+	$path = $APPLICATION->GetCurPage();
+	$path = explode("/", $path);
 
-    if(count($path) >= 4 && $path[3]){
-        $section_code = $path[3];
-        
-        $rs_section = CIBlockSection::GetList(
-            array(),
-            array( 
-                "IBLOCK_TYPE" => "lists",
-                "IBLOCK_CODE" => "catalog",
-                "CODE" => $section_code
-            )
-        );
-        $ar_secrion = $rs_section->GetNext();
-        
-        $arFilter["SECTION_ID"] = $ar_secrion["ID"];
-    }
+	if(count($path) >= 4 && $path[3]){
+		$section_code = $path[3];
+
+		$rs_section = CIBlockSection::GetList(
+			array(),
+			array(
+				"IBLOCK_TYPE" => "lists",
+				"IBLOCK_CODE" => "catalog",
+				"CODE" => $section_code
+			)
+		);
+		$ar_secrion = $rs_section->GetNext();
+
+		$arFilter["SECTION_ID"] = $ar_secrion["ID"];
+	}
 }
 if($arrFilter["PROPERTY_FOR"]){
-    $arFilter["PROPERTY_FOR"] = $arrFilter["PROPERTY_FOR"];
+	$arFilter["PROPERTY_FOR"] = $arrFilter["PROPERTY_FOR"];
 }
 
 $total = CIBlockElement::GetList(
-    array(),
-    $arFilter,
-    false,
-    array(),
-    array()
+	array(),
+	$arFilter,
+	false,
+	array(),
+	array()
 );
 
 $totalcount = $total->SelectedRowsCount();
 
 if(count($arResult["ITEMS"]) && $onPage < $totalcount){?>
-    <a class="load_more" title="<?=GetMessage("SHOW_MORE")?>" data-next-page="2" data-count="<?=$itemCount?>" data-iblock-section="<?=$arFilter["SECTION_ID"]?>" data-brand="<?=$currentBrendID?>" data-for="<?=$arrFilter["PROPERTY_FOR"]?>" data-iblock="catalog"><span><?=GetMessage("SHOW_MORE")?></span></a><?
-}
-
-?>
+	<a class="load_more" title="<?=GetMessage("SHOW_MORE")
+		?>" data-next-page="2" data-count="<?=$itemCount
+		?>" data-iblock-section="<?=$arFilter["SECTION_ID"]
+		?>" data-brand="<?=$currentBrendID
+		?>" data-for="<?=$arrFilter["PROPERTY_FOR"]
+		?>" data-iblock="catalog"><span><?=GetMessage("SHOW_MORE")
+		?></span></a>
+<?}?>

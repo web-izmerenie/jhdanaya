24dd1d13ebb5e11517c4801fd2360d09e3062bb0
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-26 03:15:21 +0400

    fixed "blur_img" basics module for support commonjs and refactored for jshint

--- bitrix/templates/main/scripts/src/basics/blur_img.js
+++ bitrix/templates/main/scripts/src/basics/blur_img.js
@@ -1,23 +1,70 @@
-/*!
+/**
  * Get blured image
  *
  * @module blur_img
  * @requires load_img
  *
- * @version r1
+ * @version r2
  * @author Viacheslav Lotsmanov
  * @license GNU/GPLv3 by Free Software Foundation (https://github.com/unclechu/js-useful-amd-modules/blob/master/GPLv3-LICENSE)
  * @see {@link https://github.com/unclechu/js-useful-amd-modules/|GitHub}
  * @copyright Based on https://github.com/jakiestfu/Blur.js
  */
 
-define(['jquery', 'load_img'], function ($, loadImg) {
+(function (factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD (RequireJS)
+		define(['jquery', 'load_img'], function ($, loadImg) {
+			factory($, loadImg, window, window.document);
+		});
+	} else if (typeof module === 'object' && typeof module.exports === 'object') {
+		// CommonJS (Browserify)
+		module.exports = factory(
+			require('jquery'), require('load_img'), window, window.document);
+	} else {
+		throw new Error('Unsupported architecture');
+	}
+})(function ($, loadImg, window, document, undefined) {
+
+	'use strict';
 
 	// helpers {{{1
 
 	// Stackblur, courtesy of Mario Klingemann: http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
-	var mul_table = [512, 512, 456, 512, 328, 456, 335, 512, 405, 328, 271, 456, 388, 335, 292, 512, 454, 405, 364, 328, 298, 271, 496, 456, 420, 388, 360, 335, 312, 292, 273, 512, 482, 454, 428, 405, 383, 364, 345, 328, 312, 298, 284, 271, 259, 496, 475, 456, 437, 420, 404, 388, 374, 360, 347, 335, 323, 312, 302, 292, 282, 273, 265, 512, 497, 482, 468, 454, 441, 428, 417, 405, 394, 383, 373, 364, 354, 345, 337, 328, 320, 312, 305, 298, 291, 284, 278, 271, 265, 259, 507, 496, 485, 475, 465, 456, 446, 437, 428, 420, 412, 404, 396, 388, 381, 374, 367, 360, 354, 347, 341, 335, 329, 323, 318, 312, 307, 302, 297, 292, 287, 282, 278, 273, 269, 265, 261, 512, 505, 497, 489, 482, 475, 468, 461, 454, 447, 441, 435, 428, 422, 417, 411, 405, 399, 394, 389, 383, 378, 373, 368, 364, 359, 354, 350, 345, 341, 337, 332, 328, 324, 320, 316, 312, 309, 305, 301, 298, 294, 291, 287, 284, 281, 278, 274, 271, 268, 265, 262, 259, 257, 507, 501, 496, 491, 485, 480, 475, 470, 465, 460, 456, 451, 446, 442, 437, 433, 428, 424, 420, 416, 412, 408, 404, 400, 396, 392, 388, 385, 381, 377, 374, 370, 367, 363, 360, 357, 354, 350, 347, 344, 341, 338, 335, 332, 329, 326, 323, 320, 318, 315, 312, 310, 307, 304, 302, 299, 297, 294, 292, 289, 287, 285, 282, 280, 278, 275, 273, 271, 269, 267, 265, 263, 261, 259];
-	var shg_table = [9, 11, 12, 13, 13, 14, 14, 15, 15, 15, 15, 16, 16, 16, 16, 17, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24];
+	var mul_table = [512, 512, 456, 512, 328, 456, 335, 512, 405, 328, 271, 456,
+		388, 335, 292, 512, 454, 405, 364, 328, 298, 271, 496, 456, 420, 388,
+		360, 335, 312, 292, 273, 512, 482, 454, 428, 405, 383, 364, 345, 328,
+		312, 298, 284, 271, 259, 496, 475, 456, 437, 420, 404, 388, 374, 360,
+		347, 335, 323, 312, 302, 292, 282, 273, 265, 512, 497, 482, 468, 454,
+		441, 428, 417, 405, 394, 383, 373, 364, 354, 345, 337, 328, 320, 312,
+		305, 298, 291, 284, 278, 271, 265, 259, 507, 496, 485, 475, 465, 456,
+		446, 437, 428, 420, 412, 404, 396, 388, 381, 374, 367, 360, 354, 347,
+		341, 335, 329, 323, 318, 312, 307, 302, 297, 292, 287, 282, 278, 273,
+		269, 265, 261, 512, 505, 497, 489, 482, 475, 468, 461, 454, 447, 441,
+		435, 428, 422, 417, 411, 405, 399, 394, 389, 383, 378, 373, 368, 364,
+		359, 354, 350, 345, 341, 337, 332, 328, 324, 320, 316, 312, 309, 305,
+		301, 298, 294, 291, 287, 284, 281, 278, 274, 271, 268, 265, 262, 259,
+		257, 507, 501, 496, 491, 485, 480, 475, 470, 465, 460, 456, 451, 446,
+		442, 437, 433, 428, 424, 420, 416, 412, 408, 404, 400, 396, 392, 388,
+		385, 381, 377, 374, 370, 367, 363, 360, 357, 354, 350, 347, 344, 341,
+		338, 335, 332, 329, 326, 323, 320, 318, 315, 312, 310, 307, 304, 302,
+		299, 297, 294, 292, 289, 287, 285, 282, 280, 278, 275, 273, 271, 269,
+		267, 265, 263, 261, 259];
+	var shg_table = [9, 11, 12, 13, 13, 14, 14, 15, 15, 15, 15, 16, 16, 16, 16,
+		17, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19, 19,
+		19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20, 20, 20, 20,
+		20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 21, 21, 21, 21, 21, 21,
+		21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21,
+		21, 21, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
+		22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
+		22, 22, 22, 22, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+		23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+		23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+		23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+		24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+		24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+		24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+		24, 24, 24, 24, 24, 24];
 
 	function inherit(proto) {
 		if (Object.create) return Object.create(proto);
@@ -34,13 +81,11 @@ define(['jquery', 'load_img'], function ($, loadImg) {
 	 */
 	function stackBlurCanvasRGB(canvas, b, c, width, height, radius) {
 		var a = canvas;
-		var b = b;
-		var c = c;
 		var d = width;
 		var f = height;
 		var g = radius;
 
-		if(isNaN(g) || g < 1) return;
+		if (isNaN(g) || g < 1) return;
 		g |= 0;
 		var h = a.getContext("2d");
 		var j;
@@ -49,30 +94,33 @@ define(['jquery', 'load_img'], function ($, loadImg) {
 				j = h.getImageData(b, c, d, f);
 			} catch(e) {
 				try {
-					netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
+					window.netscape.security.PrivilegeManager
+						.enablePrivilege("UniversalBrowserRead");
 					j = h.getImageData(b, c, d, f);
 				} catch(e) {
-					alert("Cannot access local image");
+					window.alert("Cannot access local image");
 					throw new Error("unable to access local image data: " + e);
 				}
 			}
 		} catch(e) {
-			alert("Cannot access image");
+			window.alert("Cannot access image");
 			throw new Error("unable to access image data: " + e);
 		}
 		var k = j.data;
-		var x, y, i, p, yp, yi, yw, r_sum, g_sum, b_sum, r_out_sum, g_out_sum, b_out_sum, r_in_sum, g_in_sum, b_in_sum, pr, pg, pb, rbs;
+		var x, y, i, p, yp, yi, yw, r_sum, g_sum, b_sum, r_out_sum, g_out_sum,
+			b_out_sum, r_in_sum, g_in_sum, b_in_sum, pr, pg, pb, rbs;
 		var l = g + g + 1;
-		var m = d << 2;
+		//var m = d << 2;
 		var n = d - 1;
 		var o = f - 1;
 		var q = g + 1;
 		var r = q * (q + 1) / 2;
 		var s = new BlurStack();
 		var t = s;
+		var u;
 		for(i = 1; i < l; i++) {
 			t = t.next = new BlurStack();
-			if(i == q) var u = t;
+			if(i == q) u = t;
 		}
 		t.next = s;
 		var v = null;
@@ -242,7 +290,7 @@ define(['jquery', 'load_img'], function ($, loadImg) {
 		}
 
 		var canvas = document.createElement('canvas');
-		var ctx, imgObj, $img, imgPixels, dataURL;
+		var ctx, imgObj, dataURL;
 
 		if (canvas.getContext) {
 			ctx = canvas.getContext('2d');
@@ -297,7 +345,7 @@ define(['jquery', 'load_img'], function ($, loadImg) {
 	blurImg.exceptions = {};
 	var exceptions = blurImg.exceptions;
 
-	exceptions.NoImage = function (message, type) {
+	exceptions.NoImage = function (message) {
 		Error.call(this);
 		this.name = 'NoImage';
 		this.message = message || 'No "Image" constructor.';
@@ -331,4 +379,4 @@ define(['jquery', 'load_img'], function ($, loadImg) {
 
 	return blurImg;
 
-}); // define()
+}); // factory()

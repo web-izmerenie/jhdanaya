f5eca913f41ce479fdbc180c7f001eb54930d156
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-26 04:02:41 +0400

    added commonjs support to "json_answer"

--- bitrix/templates/main/scripts/src/basics/json_answer.js
+++ bitrix/templates/main/scripts/src/basics/json_answer.js
@@ -1,16 +1,38 @@
 /*!
  * Check JSON data for correct and "status" is "success"
  *
- * @version r3
+ * @version r4
  * @author Viacheslav Lotsmanov
  * @license GNU/GPLv3 by Free Software Foundation (https://github.com/unclechu/js-useful-amd-modules/blob/master/GPLv3-LICENSE)
  * @see {@link https://github.com/unclechu/js-useful-amd-modules/|GitHub}
  */
 
-define(function () {
+(function (factory) {
+	if (typeof define === 'function' && define.amd)
+		define(factory); // AMD (RequireJS)
+	else if (typeof module === 'object' && typeof module.exports === 'object')
+		module.exports = factory(); // CommonJS (Browserify)
+	else throw new Error('Unsupported architecture');
+})(function () {
 
-	/** @public */
-	var exports = {};
+	'use strict';
+
+	// helpers {{{1
+
+	/**
+	 * @private
+	 * @inner
+	 */
+	function inherit(proto) {
+		if (Object.create) return Object.create(proto);
+		function F() {}
+		F.prototype = proto;
+		return new F();
+	}
+
+	// helpers }}}1
+
+	/** @public */ var exports = {};
 
 	/**
 	 * @private
@@ -19,13 +41,7 @@ define(function () {
 	 * @param {Error} err
 	 */
 	function makeError(err, callback) {
-
-		if (callback) {
-			callback(err);
-		} else {
-			throw err;
-		}
-
+		if (callback) callback(err); else throw err;
 	}
 
 	/**
@@ -34,7 +50,7 @@ define(function () {
 	 * @param {string} jsonData JSON string
 	 * @param {callback} [callback] Callback for async return value
 	 */
-	function validate(jsonData, callback) {
+	function validate(jsonData, callback) { // {{{1
 
 		if (
 			typeof callback !== 'function' &&
@@ -43,12 +59,11 @@ define(function () {
 			throw new exports.exceptions.IncorrectCallbackArgument(null, typeof(callback));
 		}
 
-		if (typeof jsonData !== 'string') {
+		if (typeof jsonData !== 'string')
 			return makeError(
 				new exports.exceptions.IncorrectJSONDataArgument(null, typeof(jsonData)),
 				callback
 			);
-		}
 
 		var parsed;
 
@@ -59,51 +74,43 @@ define(function () {
 			return makeError(new exports.exceptions.ParseJSONError(null, err), callback);
 		}
 
-		if (!('status' in parsed)) {
+		if (!('status' in parsed))
 			return makeError(new exports.exceptions.NoStatusKey(), callback);
-		}
 
-		if (typeof parsed.status !== 'string') {
+		if (typeof parsed.status !== 'string')
 			return makeError(
 				new exports.exceptions.IncorrectStatusType(null, typeof(parsed.status)),
 				callback
 			);
-		}
 
-		if (parsed.status !== 'error' && parsed.status !== 'success') {
+		if (parsed.status !== 'error' && parsed.status !== 'success')
 			return makeError(
 				new exports.exceptions.UnknownStatusValue(null, parsed.status, parsed),
 				callback
 			);
-		}
 
-		if (parsed.status === 'error') {
+		if (parsed.status === 'error')
 			return makeError(
 				new exports.exceptions.ErrorStatus(null, parsed),
 				callback
 			);
-		}
 
-		if (callback) {
-			setTimeout(function async() {
-				callback(null, parsed);
-			}, 1);
-		} else {
+		if (callback)
+			setTimeout(function () { callback(null, parsed); }, 0);
+		else
 			return parsed;
-		}
 
-	}
+	} // validate() }}}1
 
 	/* exceptions {{{1 */
 
 	/**
-	 * @public 
+	 * @public
 	 * @static
 	 */
 	exports.exceptions = {};
 
-	exports.exceptions.IncorrectJSONDataArgument =
-	function IncorrectJSONDataArgument(message, type) {
+	exports.exceptions.IncorrectJSONDataArgument = function (message, type) { // {{{2
 		Error.call(this);
 		this.name = 'IncorrectJSONDataArgument';
 		if (message) {
@@ -111,13 +118,12 @@ define(function () {
 		} else {
 			this.message = 'Incorrect type of JSON data argument';
 			if (type) this.message += ' ("'+ type +'")';
-			this.message += ', must be a "string".';
+			this.message += ', must be a "string"';
 		}
 		if (type) this.type = type;
-	};
+	}; // }}}2
 
-	exports.exceptions.IncorrectCallbackArgument =
-	function IncorrectCallbackArgument(message, type) {
+	exports.exceptions.IncorrectCallbackArgument = function (message, type) { // {{{2
 		Error.call(this);
 		this.name = 'IncorrectCallbackArgument';
 		if (message) {
@@ -125,31 +131,28 @@ define(function () {
 		} else {
 			this.message = 'Incorrect callback argument';
 			if (type) this.message += ' ("'+ type +'")';
-			this.message += ', must be a "function".';
+			this.message += ', must be a "function"';
 		}
 		if (type) this.type = type;
-	};
+	}; // }}}2
 
-	exports.exceptions.ParseJSONError =
-	function ParseJSONError(message, err) {
+	exports.exceptions.ParseJSONError = function (message, err) { // {{{2
 		Error.call(this);
 		this.name = 'ParseJSONError';
-		this.message = message || 'Parse JSON data error.';
+		this.message = message || 'Parse JSON data error';
 		if (err) {
 			this.err = err;
-			if (!message) this.message += '\n' + err.toString();
+			if (!message) this.message += '\n\n' + err.toString();
 		}
-	};
+	}; // }}}2
 
-	exports.exceptions.NoStatusKey =
-	function NoStatusKey(message) {
+	exports.exceptions.NoStatusKey = function (message) { // {{{2
 		Error.call(this);
 		this.name = 'NoStatusKey';
-		this.message = message || 'No "status" key in JSON data.';
-	};
+		this.message = message || 'No "status" key in JSON data';
+	}; // }}}2
 
-	exports.exceptions.IncorrectStatusType =
-	function IncorrectStatusType(message, type) {
+	exports.exceptions.IncorrectStatusType = function (message, type) { // {{{2
 		Error.call(this);
 		this.name = 'IncorrectStatusType';
 		if (message) {
@@ -157,13 +160,12 @@ define(function () {
 		} else {
 			this.message = 'Incorrect type of value of "status" key';
 			if (type) this.message += ' ("'+ type +'")';
-			this.message += ', must be a "string".';
+			this.message += ', must be a "string"';
 		}
 		if (type) this.type = type;
-	};
+	}; // }}}2
 
-	exports.exceptions.UnknownStatusValue =
-	function UnknownStatusValue(message, val, json) {
+	exports.exceptions.UnknownStatusValue = function (message, val, json) { // {{{2
 		Error.call(this);
 		this.name = 'UnknownStatusValue';
 		if (message) {
@@ -171,30 +173,20 @@ define(function () {
 		} else {
 			this.message = 'Unknown value of "status" key';
 			if (val) this.message += ': "'+ val +'"';
-			this.message += '.';
 		}
 		if (val) this.value = val;
 		if (json) this.json = json;
-	};
+	}; // }}}2
 
-	exports.exceptions.ErrorStatus =
-	function ErrorStatus(message, json) {
+	exports.exceptions.ErrorStatus = function (message, json) { // {{{2
 		Error.call(this);
 		this.name = 'ErrorStatus';
-		this.message = message || 'Value of "status" key is "error".';
+		this.message = message || 'Value of "status" key is "error"';
 		if (json) this.json = json;
-	};
+	}; // }}}2
 
-	function inherit(proto) {
-		if (Object.create) return Object.create(proto);
-		function F() {}
-		F.prototype = proto;
-		return new F();
-	}
-
-	for (var key in exports.exceptions) {
+	for (var key in exports.exceptions)
 		exports.exceptions[key].prototype = inherit(Error.prototype);
-	}
 
 	/* exceptions }}}1 */
 
@@ -202,4 +194,4 @@ define(function () {
 
 	return exports;
 
-});
+}); // factory()

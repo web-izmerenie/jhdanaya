75138add64ea421aab5815038c43e5cf5d35a24b
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-27 22:14:01 +0400

    removed grayscaling preview photos in "events" page list

--- bitrix/templates/main/scripts/src/pages/events.js
+++ bitrix/templates/main/scripts/src/pages/events.js
@@ -17,30 +17,6 @@ ready(function (window, document, undefined) {
 
 	var getVal = basics.getVal;
 
-	$eventsList.find('.event_item ul.preview_photos li img').each(function () {
-		var $img = $(this);
-		var src = $img.attr('src');
-
-		function grayscale() {
-			var loadImg = require('load_img');
-			var grayscaleImg = require('../basics/grayscale_img');
-			grayscaleImg(src, function (err, dataURL) {
-				if (err) {
-					if (err instanceof loadImg.exceptions.Timeout) {
-						setTimeout(grayscale, 0); // try again
-						return;
-					}
-					require('../just_log_error')(err);
-					return;
-				}
-
-				$img.attr('src', dataURL).addClass('finished');
-			}); // grayscaleImg()
-		} // grayscale()
-
-		setTimeout(grayscale, 0);
-	});
-
 	var $photos = $('html.event_detail_page body main section.event_detail');
 	$photos = $photos.find('ul.preview_photos>li a');
 

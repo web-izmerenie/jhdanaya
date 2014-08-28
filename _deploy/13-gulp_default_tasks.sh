52bff8e2ba0f897a6c09e4eae87dbdc70481997f
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-28 09:07:34 +0400

    production flag for deploy gulp caller

--- _deploy/13-gulp_default_tasks.sh
+++ _deploy/13-gulp_default_tasks.sh
@@ -2,6 +2,6 @@
 if [ -z "$PARENT_DEPLOY_SCRIPT" ]; then YOUR_SUBJECT="./_deploy/$(basename "$0")" WD="$(dirname "$0")/../" ../deploy.sh; exit "$?"; fi
 
 info "Starting default gulp tasks"
-if ./gulp; then
+if ./gulp --production; then
 	info_inline "Default gulp tasks status:"; ok;
 else err; fi

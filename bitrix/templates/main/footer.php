435b8d86e866bd8ef40b16d3a0a7b49f4b78f22f
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-12-29 08:26:34 +0300

    added social buttons to footer

--- bitrix/templates/main/footer.php
+++ bitrix/templates/main/footer.php
@@ -1,5 +1,17 @@
 	<?if(!defined('ERROR_404')){?></main><?}?>
 	<footer>
+		<div class="social">
+			<a
+				href="http://instagram.com/jhdanaya/"
+				class="instagram"
+				title="Instagram"
+				target="_blank">Instagram</a>
+			<a
+				href="https://www.facebook.com/pages/%D0%AE%D0%B2%D0%B5%D0%BB%D0%B8%D1%80%D0%BD%D1%8B%D0%B9-%D0%B4%D0%BE%D0%BC-%D0%94%D0%B0%D0%BD%D0%B0%D1%8F/607619165995029"
+				class="fb"
+				title="Facebook"
+				target="_blank">Facebook</a>
+		</div>
 		<div class="developer">
 			<?=GetMessage("DEVELOPER")?>
 		</div>
